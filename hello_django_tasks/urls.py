dc90878bd0025cfec4cec20c0d3f083863f9480a
Richard Caceres <rchrdccrs@gmail.com>
2023-08-11 15:08:22 -0700

    Simply the code by making some assumptions. Eg pass dict

--- hello_django_tasks/urls.py
+++ hello_django_tasks/urls.py
@@ -8,58 +8,42 @@ from google.protobuf import timestamp_pb2
 import datetime
 import json
 
-def create_task(project=None, queue=None, location=None, payload=None, in_seconds=None):
+# TODO(developer): Get these from settings.py / ENV
+PROJECT = 'hello-tasks-395419'
+QUEUE = 'my-queue'
+LOCATION = 'us-west1'
+
+def create_task(url, payload={}):
+    """
+    Create a task for a given queue with an arbitrary payload.
+
+    payload (dict): The payload of the task.
+    """
     client = tasks_v2.CloudTasksClient()
-
-    # TODO(developer): Get these from settings.py / ENV
-    project = 'hello-tasks-395419'
-    queue = 'my-queue'
-    location = 'us-west1'
-    payload = 'hello' # or {'param': 'value'} for application/json
-    in_seconds = None
-
-    parent = client.queue_path(project, location, queue)
-
+    parent = client.queue_path(PROJECT, LOCATION, QUEUE)
     task = {
         "app_engine_http_request": {
-            "http_method": 'POST',
-            "relative_uri": reverse("example_task_handler"),
+            "http_method": "POST",
+            "relative_uri": url,
+            "headers": {
+                "Content-type": "application/json"
+            },
+            "body": json.dumps(payload).encode()
         }
     }
-
-    if payload is not None:
-        if isinstance(payload, dict):
-            payload = json.dumps(payload)
-            task["app_engine_http_request"]["headers"] = {
-                "Content-type": "application/json"
-            }
-        # The API expects a payload of type bytes.
-        converted_payload = payload.encode()
-        task["app_engine_http_request"]["body"] = converted_payload
-
-    if in_seconds is not None:
-        # Convert "seconds from now" into an rfc3339 datetime string.
-        d = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(
-            seconds=in_seconds
-        )
-        timestamp = timestamp_pb2.Timestamp()
-        timestamp.FromDatetime(d)
-        task["schedule_time"] = timestamp
-
     response = client.create_task(parent=parent, task=task)
-
     print(f"Created task {response.name}")
     return response
 
 class CreateTaskView(View):
     def get(self, request):
-        create_task()
+        create_task(reverse("example_task_handler"), payload={"message": "Hello World"})
         return HttpResponse('Task created')
 
 
 class ExampleTaskHandlerView(View):
     def post(self, request):
-        payload = request.body
+        payload = json.loads(request.body)
         print(f"Received task with payload: {payload}")
         return HttpResponse('Task created')
 
