91646b0fb316d98a8e26d386bee27f0e94cecdb2
Richard Caceres <rchrdccrs@gmail.com>
2023-08-11 13:50:47 -0700

    It works

--- hello_django_tasks/urls.py
+++ hello_django_tasks/urls.py
@@ -1,23 +1,71 @@
-"""
-URL configuration for hello_django_tasks project.
+from django.urls import path, reverse
+from django.views import View
+from django.http import HttpResponse
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# from django.contrib import admin
-from django.urls import path, include
+from google.cloud import tasks_v2
+from google.protobuf import timestamp_pb2
+
+import datetime
+import json
+
+def create_task(project=None, queue=None, location=None, payload=None, in_seconds=None):
+    client = tasks_v2.CloudTasksClient()
+
+    # TODO(developer): Get these from settings.py / ENV
+    project = 'hello-tasks-395419'
+    queue = 'my-queue'
+    location = 'us-west1'
+    payload = 'hello' # or {'param': 'value'} for application/json
+    in_seconds = None
+
+    parent = client.queue_path(project, location, queue)
+
+    task = {
+        "app_engine_http_request": {
+            "http_method": tasks_v2.HttpMethod.POST,
+            "relative_uri": reverse("example_task_handler"),
+        }
+    }
+
+    if payload is not None:
+        if isinstance(payload, dict):
+            payload = json.dumps(payload)
+            task["app_engine_http_request"]["headers"] = {
+                "Content-type": "application/json"
+            }
+        # The API expects a payload of type bytes.
+        converted_payload = payload.encode()
+        task["app_engine_http_request"]["body"] = converted_payload
+
+    if in_seconds is not None:
+        # Convert "seconds from now" into an rfc3339 datetime string.
+        d = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(
+            seconds=in_seconds
+        )
+        timestamp = timestamp_pb2.Timestamp()
+        timestamp.FromDatetime(d)
+        task["schedule_time"] = timestamp
+
+    response = client.create_task(parent=parent, task=task)
+
+    print(f"Created task {response.name}")
+    return response
+
+class CreateTaskView(View):
+    def get(self, request):
+        create_task()
+        return HttpResponse('Task created')
+
+
+class ExampleTaskHandlerView(View):
+    def post(self, request):
+        payload = request.body
+        print(f"Received task with payload: {payload}")
+        return HttpResponse('Task created')
+
+
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-
+    path('create_task/', CreateTaskView.as_view(), name='create_task'),
+    path('example_task_handler/', ExampleTaskHandlerView.as_view(), name='example_task_handler'),
 ]
